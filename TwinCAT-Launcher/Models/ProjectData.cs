78baa116207163fcabce665e0b097f55cbcbee0e
Chris <chriskng84@gmail.com>
2022-12-19 22:26:55 +0000

    added projectdata model and implemented in MainViewModel

---
+++ TwinCAT-Launcher/Models/ProjectData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinCAT_Launcher.Models
+{
+    public class ProjectData
+    {
+        public string Directory { get; set; }
+        public string Name { get; set; }        
+
+    }
+}
--- TwinCAT-Launcher/ViewModels/MainViewModel.cs
+++ TwinCAT-Launcher/ViewModels/MainViewModel.cs
@@ -1,24 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 using Engine;
 using TwinCAT_Launcher.Commands;
+using TwinCAT_Launcher.Models;
 
 namespace TwinCAT_Launcher.ViewModels
 {
     class MainViewModel
-    {
+    {   
         VisualStudioHandler studioHandler;
         public CreateProjectCommand createProjectCommand;
+        public ProjectData projectData { get; set; }
 
         public MainViewModel()
         {
             studioHandler= new VisualStudioHandler();
             studioHandler.InitialiseVSEnv();
+            projectData = new ProjectData();
             createProjectCommand = new CreateProjectCommand(studioHandler);
+            
         }
 
     }
