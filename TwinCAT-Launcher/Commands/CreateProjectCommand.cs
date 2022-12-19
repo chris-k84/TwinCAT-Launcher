698e283ed500ef7b6239a660fba882a5f4bd3e72
Chris <chriskng84@gmail.com>
2022-12-19 14:41:19 +0000

    added command for project build, added viewmodel for main view

---
+++ TwinCAT-Launcher/Commands/CreateProjectCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Engine;
+
+namespace TwinCAT_Launcher.Commands
+{
+    class CreateProjectCommand : ICommand
+    {
+        VisualStudioHandler VisualStudioHandler { get; set; }
+        public CreateProjectCommand(VisualStudioHandler visualStudioHandler)
+        {
+            this.VisualStudioHandler = visualStudioHandler;
+        }
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+
+            VisualStudioHandler.CreateDirectory(@"C:\Users\Chris\Desktop\TestProject");
+            VisualStudioHandler.CreateSolution("Test");
+
+            VisualStudioHandler.CreateTCProj("Test");
+
+            VisualStudioHandler.Save();
+        }
+    }
+}
---
+++ TwinCAT-Launcher/ViewModels/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Engine;
+using TwinCAT_Launcher.Commands;
+
+namespace TwinCAT_Launcher.ViewModels
+{
+    class MainViewModel
+    {
+        VisualStudioHandler studioHandler;
+        public CreateProjectCommand createProjectCommand;
+
+        public MainViewModel()
+        {
+            studioHandler= new VisualStudioHandler();
+            studioHandler.InitialiseVSEnv();
+            createProjectCommand = new CreateProjectCommand(studioHandler);
+        }
+
+    }
+}
