ee82bd6a7c142a401fb61885b9ce0267c6839d6d
Chris <chriskng84@gmail.com>
2023-01-10 01:16:10 +0000

    modified the ProjectViewModel to use the relaycommand to create a project, refactored through the MainView

--- TwinCAT-Launcher/MVVM/ViewModels/ProjectViewModel.cs
+++ TwinCAT-Launcher/MVVM/ViewModels/ProjectViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwinCAT_Launcher.Commands;
+using TwinCAT_Launcher.Core;
 using TwinCAT_Launcher.MVVM.Models;
 
 namespace TwinCAT_Launcher.MVVM.ViewModels
@@ -12,7 +13,8 @@ namespace TwinCAT_Launcher.MVVM.ViewModels
     class ProjectViewModel
     {
         VisualStudioHandler studioHandler;
-        public CreateProjectCommand createProjectCommand { get; set; }
+        //public CreateProjectCommand createProjectCommand { get; set; }
+        public RelayCommand CreateProjectCommand { get; set; }  
         public ProjectData projectData { get; set; }
 
         public ProjectViewModel()
@@ -22,7 +24,17 @@ namespace TwinCAT_Launcher.MVVM.ViewModels
             projectData = new ProjectData();
             projectData.Name = "TestProject";
             projectData.Directory = @"C:\Users\Chris\Documents\Projects\";
-            createProjectCommand = new CreateProjectCommand(studioHandler);
+            CreateProjectCommand = new RelayCommand(CreateProject);
+        }
+
+        public void CreateProject(object parameter)
+        {
+            ProjectData projectData = (ProjectData)parameter;
+            studioHandler.SetEnvVisability(true, true);
+            studioHandler.CreateDirectory(projectData.Directory);
+            studioHandler.CreateSolution(projectData.Name);
+            studioHandler.CreateTCProj(projectData.Name);
+            studioHandler.Save();
         }
     }
 }
