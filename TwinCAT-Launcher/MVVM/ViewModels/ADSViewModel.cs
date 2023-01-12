b43a13e261ee2a6e9464068575b28301921edbb9
Chris <chriskng84@gmail.com>
2023-01-12 22:28:08 +0000

    added implementation for the ADSViewModel, commands for the buttons and a Routes property to hold the returns ADS routes from a scan

--- TwinCAT-Launcher/MVVM/ViewModels/ADSViewModel.cs
+++ TwinCAT-Launcher/MVVM/ViewModels/ADSViewModel.cs
@@ -1,12 +1,32 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TwinCAT_Launcher.Core;
+using TwinCAT_Launcher.MVVM.Models;
 
 namespace TwinCAT_Launcher.MVVM.ViewModels
 {
     class ADSViewModel
     {
+        private string _routes { get; set; }
+        private AdsHandler AdsHandler;
+        public string Routes { get; set; }
+
+        public RelayCommand ScanAdsServersCommand;
+        public RelayCommand AddRouteCommand;
+
+        public ADSViewModel()
+        {
+            AdsHandler = new AdsHandler();
+            ScanAdsServersCommand = new RelayCommand(ScanAdsServers);
+        }
+
+        public void ScanAdsServers(object parameter)
+        {
+            Routes = AdsHandler.ScanADSDevices();
+        }
     }
 }
